7849b4a26987a23006e6a13d21ec8c1a8bfba52c
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-09-13 13:35:39 +0200

    part. 1 migration completed

---
+++ database/migrations/2022_09_13_095350_create_user_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("user_id");
+            $table->foreign("user_id")->references("id")->on("users");
+            $table->string("address", 50);
+            $table->string("city", 50);
+            $table->string("VAT_number", 11);
+            $table->text("img_thumb")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_details');
+    }
+}
---
+++ database/migrations/2022_09_13_113027_create_product_category_product_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductCategoryProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_category_product', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("product_category_id");
+            $table->foreign("product_category_id")->references("id")->on("products_categories");
+
+            $table->unsignedBigInteger("product_id");
+            $table->foreign("product_id")->references("id")->on("products");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_category_product');
+    }
+}
