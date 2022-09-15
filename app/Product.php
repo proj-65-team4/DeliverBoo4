53051d1dff9847fb79588ec97f75966cb31853fc
erica201193 <erica.mancini20@gmail.com>
2022-09-15 16:39:57 +0200

    Product_category model

--- app/Product.php
+++ app/Product.php
@@ -9,4 +9,9 @@ class Product extends Model
     public function orders () {
         return $this->belongsToMany("App\Product");
     }
+
+    public function product_category()
+    {
+        return $this->belongsToMany("App\Product_category");
+    }
 }
