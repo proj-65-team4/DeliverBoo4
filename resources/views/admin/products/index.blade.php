ab053914a44fc2ffbb69ae82c887128427ca2caf
erica201193 <erica.mancini20@gmail.com>
2022-09-16 16:44:11 +0200

    Index view initial

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -2,37 +2,24 @@
 
 @section('content')
 <div class="container">
+    
     <div class="row justify-content-center">
         <div class="col-12">
             <h1>I tuoi prodotti</h1>
             <a href="{{ route('admin.products.create') }}" class="btn btn-success">
                 Aggiungi prodotto
             </a>
-            
-            <table>
-                <thead>
-                    <tr>
-                        <th>Immagine</th>
-                        <th>Nome</th>
-                        <th>Prezzo</th>
-                        <th>Visibile</th>
-                        <th>Disponibile</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($products as $product)
 
-                    <tr>
-                        <td><img class="img-fluid" src="{{ $product->image }}" alt=""></td>
-                        <td>{{ $product->name }}</td>
-                        <td>{{ $product->price }}</td>
-                        <td>{{ $product->visible }}</td>
-                        <td>{{ $product->available }}</td>
-                    </tr>
-                        
-                    @endforeach
-                </tbody>
-            </table>
+            @foreach ($products as $product)
+
+            <div class="col-12">
+                <img class="img-fluid" src="{{ $product->image }}" alt="">
+                <div>Nome: {{ $product->name }}</div>
+                <div>Prezzo: {{ $product->price }}</div>
+                <div>Visibile: {{ $product->visible }}</div>
+                <div>Disponibile: {{ $product->available }}</div>
+            </div>
+            @endforeach
         </div>
     </div>
 </div>
