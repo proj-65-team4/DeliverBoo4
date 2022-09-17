d5450719abc27609958e0b8a3649f84972664747
Michael Bombini <bombini.michael@gmail.com>
2022-09-17 11:16:03 +0200

    Added bootstrap css to vue

--- resources/js/frontend.js
+++ resources/js/frontend.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import VueRouter from "vue-router";
 import { routes } from "./routes"
 import "bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 Vue.use(VueRouter);
 
