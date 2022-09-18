3f60dd34a37a302d7e59d0e522bb6aea5a01d754
felicetto13 <felice.laterza93@icloud.com>
2022-09-18 15:57:09 +0200

    commit

--- public/js/app.js
+++ public/js/app.js
@@ -53777,15 +53777,27 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/sass/backend.scss":
+/*!*************************************!*\
+  !*** ./resources/sass/backend.scss ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nError: ENOENT: no such file or directory, open '/Users/felicelaterza/boolean/DeliverBoo4/resources/sass/backend.scss'\n    at /Users/felicelaterza/boolean/DeliverBoo4/node_modules/webpack/lib/NormalModule.js:316:20\n    at /Users/felicelaterza/boolean/DeliverBoo4/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /Users/felicelaterza/boolean/DeliverBoo4/node_modules/loader-runner/lib/LoaderRunner.js:203:19\n    at /Users/felicelaterza/boolean/DeliverBoo4/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:85:15\n    at process.processTicksAndRejections (node:internal/process/task_queues:77:11)");
+
+/***/ }),
+
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!*******************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/backend.scss ./resources/sass/app.scss ***!
+  \*******************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/ericamancini/Boolean/DeliverBoo4/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/ericamancini/Boolean/DeliverBoo4/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/felicelaterza/boolean/DeliverBoo4/resources/js/app.js */"./resources/js/app.js");
+__webpack_require__(/*! /Users/felicelaterza/boolean/DeliverBoo4/resources/sass/backend.scss */"./resources/sass/backend.scss");
+module.exports = __webpack_require__(/*! /Users/felicelaterza/boolean/DeliverBoo4/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
