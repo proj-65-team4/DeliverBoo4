7db7b6402f77b1a5e95715c8915ac14364e231cc
LucaSk-x <lucapiccinno64@gmail.com>
2022-09-21 13:52:35 +0200

    Completed Validation Front-end & Back-end

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -14,9 +14,9 @@
             @method('POST')
             <div class="form-container p-5">
                 <div class="form-floating mb-3">
-                    <input type="text" class="form-control" id="NameProduct"
+                    <input type="text" class="form-control" id="NameProduct" required
                       placeholder="Nome Prodotto" value="{{ old("name") }}" name="name" >
-                    <label for="NameProduct">Nome prodotto</label>
+                    <label for="NameProduct">Nome Prodotto*</label>
                     @error('name')
                     <div class="invalid-feedback alert alert-danger p-2"> 
                       {{$message}} 
@@ -28,7 +28,7 @@
                     <div class="form-floating mb-3">
                         <input type="text" class="form-control" id="ImageProduct"
                             placeholder="Immagine Prodotto" value="{{ old("image") }}" name="image" >
-                        <label for="ImageProduct">Immagine prodotto</label>
+                        <label for="ImageProduct">Immagine Prodotto</label>
                         <div class="invalid-feedback">
                             {{ $errors->first('image') }}
                         </div>
@@ -37,10 +37,10 @@
 
                 
                 <div class="form-floating mb-3">
-                  <input type="text" class="form-control {{ $errors->has('description') ? 'is-invalid' : '' }}"
+                  <input type="text" class="form-control {{ $errors->has('description') ? 'is-invalid' : '' }}" required
                     id="ProductDescription" placeholder="Descrizione Prodotto" name="description"
                     value="{{ old('description')}}">
-                  <label for="ProductDescription">Descrizione Prodotto</label>
+                  <label for="ProductDescription">Descrizione Prodotto*</label>
                   @error('description')
                     <div class="invalid-feedback alert alert-danger p-2"> 
                       {{$message}} 
@@ -49,10 +49,10 @@
                 </div>
 
                 <div class="form-floating mb-3">
-                  <input type="number" class="form-control {{ $errors->has('price') ? 'is-invalid' : '' }}"
+                  <input type="number" min="0" class="form-control {{ $errors->has('price') ? 'is-invalid' : '' }}" required
                     id="ProductPrice" placeholder="Prezzo del Prodotto" name="price"
                     value="{{ old('price') }}" step=".01">
-                  <label for="floatingInput">Prezzo del Prodotto</label>
+                  <label for="floatingInput">Prezzo del Prodotto*</label>
                   @error('price')
                     <div class="invalid-feedback alert alert-danger p-2"> 
                       {{$message}} 
@@ -61,21 +61,20 @@
                 </div>
                 
                 <div class="select-container row mb-3">
-
-                <div class="col-6 category-select flex-grow-1">
-                  <label for="categories" class="form-label">Categoria del piatto</label>
-                  <select class="form-select" name="categories[]" id="categories" multiple >
-                      <option value="">Seleziona categoria</option>
-                      @foreach ($categories as $category)
-                      <option value="{{ $category->id }}">{{-- {{ $category->id }} -  --}}
-                        {{ $category->name }}
-                      </option>
-                      @endforeach
-                  </select>
-                </div>
+                  <div class="col-6 category-select flex-grow-1">
+                    <label for="categories" class="form-label">Categoria del piatto</label>
+                    <select class="form-select" name="categories[]" id="categories" multiple >
+                        <option value="">Seleziona categoria</option>
+                        @foreach ($categories as $category)
+                        <option value="{{ $category->id }}">{{-- {{ $category->id }} -  --}}
+                          {{ $category->name }}
+                        </option>
+                        @endforeach
+                    </select>
+                  </div>
 
                   <div class="col-6 course-select flex-grow-1">
-                      <label for="product_course_id" class="form-label">Portata del piatto</label>
+                      <label for="product_course_id" class="form-label">Portata del piatto*</label>
                       <select class="form-select" name="product_course_id" id="product_course_id" required>
                           <option value="">Seleziona piatto</option>
                           @foreach ($courses as $course)
@@ -85,24 +84,27 @@
                           @endforeach
                       </select>
                   </div>
-
                 </div>
 
                 <div class="check-input d-flex gap-5">
-                    <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" name="visible" role="switch" id="ProductVisible"
-                        {{ old('visible') == 1 ? 'checked' : '' }}>
-                        <label class="form-check-label" for="ProductVisible">Prodotto Visibile</label>
-                    </div>
+                  <div class="form-check form-switch">
+                      <input class="form-check-input" type="checkbox" name="visible" role="switch" id="ProductVisible"
+                      {{ old('visible') == 1 ? 'checked' : '' }}>
+                      <label class="form-check-label" for="ProductVisible">Prodotto Visibile</label>
+                  </div>
 
-                    <div class="form-check form-switch mb-3">
-                        <input class="form-check-input" type="checkbox" name="available" role="switch"
-                            id="ProductAvailable" {{ old('available') == 1 ? 'checked' : '' }}>
-                        <label class="form-check-label" for="ProductAvailable">Prodotto Disponibile</label>
-                    </div>
+                  <div class="form-check form-switch">
+                      <input class="form-check-input" type="checkbox" name="available" role="switch"
+                          id="ProductAvailable" {{ old('available') == 1 ? 'checked' : '' }}>
+                      <label class="form-check-label" for="ProductAvailable">Prodotto Disponibile</label>
+                  </div>
                 </div>
+                <div class="text-secondary my-2">* I campi sono obbligatori</div>
 
-                <button type="submit" class="btn btn-success text-white"><i class="fa-solid fa-save"></i> Salva</button>
+                <div class="btn-submit py-2">
+                  <button type="submit" class="btn btn-success text-white"><i class="fa-solid fa-save"></i> Salva</button>
+
+                </div>
         </form>
     </div>
 @endsection
