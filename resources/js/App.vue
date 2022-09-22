630bce27459bffb3be949fba2c708f196dcc408b
erica201193 <erica.mancini20@gmail.com>
2022-09-22 11:23:56 +0200

    configuration vue

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="bg-dark container text-light display-6">
+
+        <h1>funzionaaaa</h1>
        <a href="/login">Login</a>
        <a href="/register">Register</a>
        <i class="fa-solid fa-user"></i>
