3a09aac5a4110748d283bd7d8b108a28679fd910
felicetto13 <felice.laterza93@icloud.com>
2022-09-23 10:08:50 +0200

    edited css

--- resources/views/frontend.blade.php
+++ resources/views/frontend.blade.php
@@ -9,7 +9,7 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
+    <link rel="stylesheet" href="{{ asset("css/app.css") }}">
     <!-- Styles -->
     {{-- <style>
         html,
